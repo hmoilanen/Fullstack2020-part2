e96d4f3469a8b6c7a25039d02d9d5d92900c7dc4
hmoilanen <juuso.ville.henrikki@gmail.com>
2020-07-14 19:28:04 +0300

    part 2.8 - done

--- src/App.js
+++ src/App.js
@@ -2,29 +2,42 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '123-456' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const typeContactName = (event) => {
     setNewName(event.target.value)
   }
 
+  const typeContactNumber = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   const addNewContact = (event) => {
     event.preventDefault()
 
-    const isNewName = (name) => {
-      return persons.every(person => {
-        return person.name !== name
-      })
-    }
-    
-    if (isNewName(newName)) {
-      const newPersonList = persons.concat({ name: newName })
-      setPersons(newPersonList)
-      setNewName('')
+    if (newName && newNumber) {
+      const isNewName = (name) => {
+        return persons.every(person => {
+          return person.name !== name
+        })
+      }
+      
+      if (isNewName(newName)) {
+        const newPersonList = persons.concat({
+          name: newName,
+          number: newNumber
+        })
+        setPersons(newPersonList)
+        setNewName('')
+        setNewNumber('')
+      } else {
+        alert(`${newName} is already added to phonebook!`)
+      }
     } else {
-      alert(`${newName} is already added to phonebook!`)
+      alert(`Add both name and number!`)
     }
   }
 
@@ -34,7 +47,19 @@ const App = () => {
       <form onSubmit={addNewContact}>
         <div>
           name: 
-          <input value={newName} onChange={typeContactName} placeholder="add new contact..." />
+          <input
+            value={newName}
+            onChange={typeContactName} 
+            placeholder="add new contact..."
+          />
+        </div>
+        <div>
+          number: 
+          <input
+            value={newNumber}
+            onChange={typeContactNumber} 
+            placeholder="add contact's number..." 
+          />
         </div>
         <div>
           <button type="submit">add</button>
@@ -44,7 +69,9 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map((person, index) =>
-          <li key={person.name + index}>{person.name}</li>
+          <li key={person.name + index}>
+            {person.name} {person.number ? person.number : false}
+          </li>
         )}
       </ul>
     </div>
