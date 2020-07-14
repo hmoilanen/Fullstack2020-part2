3adfb65e64e81a5fa30af2ce71ad069f041a9258
hmoilanen <juuso.ville.henrikki@gmail.com>
2020-07-14 16:50:47 +0300

    wooot... lost my changes

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-// Part 2 - course information
+// Part 2a - course information
 
 ReactDOM.render(
   <App />,
