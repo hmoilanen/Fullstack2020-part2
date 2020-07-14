5b4fa434250bafa5abf877108a61bc6d55a9f92f
hmoilanen <juuso.ville.henrikki@gmail.com>
2020-07-14 18:46:55 +0300

    part 2.6 done

--- src/App.js
+++ src/App.js
@@ -1,78 +1,42 @@
-import React from 'react'
-import Course from './components/Course'
-
-//PASKA
+import React, { useState } from 'react'
 
 const App = () => {
-  /* const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  } */
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addNewContact = (event) => {
+    event.preventDefault()
+    const newPersonList = persons.concat({ name: newName })
+    setPersons(newPersonList)
+    setNewName('')
+  }
+
+  const typeContactName = (event) => {
+    setNewName(event.target.value)
+  }
 
   return (
-    <Course courses={courses} />
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewContact}>
+        <div>
+          name: 
+          <input value={newName} onChange={typeContactName} placeholder="add new contact..." />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      {/* <div>debug: {newName}</div> */}
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, index) =>
+          <li key={person.name + index}>{person.name}</li>
+        )}
+      </ul>
+    </div>
   )
 }
 
