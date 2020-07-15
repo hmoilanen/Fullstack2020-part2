f71fb04568488f8de135c5cccb708ee7f681f382
hmoilanen <juuso.ville.henrikki@gmail.com>
2020-07-15 19:08:24 +0300

    ooopsie... still some file structure tweaks (related to part 2.18)

---
+++ src/part2d - phonebook/services/contacts.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/persons'
+
+const getAllContacts = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+const createContact = newContactObject => {
+  const request = axios.post(baseUrl, newContactObject)
+  return request.then(response => response.data)
+}
+
+const deleteContact = id => {
+  const request = axios.delete(`${baseUrl}/${id}`)
+  return request.then(response => response)
+}
+
+const updateContact = (updatedContactObject, id) => {
+  const request = axios.put(`${baseUrl}/${id}`, updatedContactObject)
+  return request.then(response => response.data)
+}
+
+export default {
+  getAllContacts,
+  createContact,
+  deleteContact,
+  updateContact
+}
