cd544ad856d4e4364a9c7cb4509ab9142b7baa65
hmoilanen <juuso.ville.henrikki@gmail.com>
2020-07-18 14:24:07 +0300

    part 3.11 - done

---
+++ src/part3b - phonebook full stack/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+// Part 3b - phonebook backend
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
